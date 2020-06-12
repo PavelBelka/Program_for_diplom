895d0a188cdcc918d37c9461dbb4325df98c7ba4
dethwyn <norzes@mail.ru>
2020-06-12 16:26:53 +0400

    fix recive event

--- Program_for_diplom/Form1.Designer.cs
+++ Program_for_diplom/Form1.Designer.cs
@@ -428,7 +428,6 @@
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Управление установкой";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
--- Program_for_diplom/Form1.cs
+++ Program_for_diplom/Form1.cs
@@ -1,24 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO.Ports;
 
-namespace Program_for_diplom
-{
-    public partial class Form1 : Form
-    {
+namespace Program_for_diplom {
+    public partial class Form1 : Form {
+        string _data = "0";
+
         SerialPort comport = new SerialPort();
         byte[] com_port_write = new byte[3];
         byte[] com_port_read = new byte[3];
         bool Read, Con, but_click = false;
-        public Form1()
-        {
+        public Form1() {
             string[] name_ports = SerialPort.GetPortNames();
             InitializeComponent();
             comboPort.Items.Clear();
@@ -27,26 +20,16 @@ namespace Program_for_diplom
             lab_statuscon.ForeColor = Color.Red;
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void comboPort_SelectedIndexChanged(object sender, EventArgs e)
-        {
+        private void comboPort_SelectedIndexChanged(object sender, EventArgs e) {
             Button_con.Enabled = Enabled;
         }
 
-        private void Button_con_Click(object sender, EventArgs e)
-        {
-            if (but_click == false)
-            {
+        private void Button_con_Click(object sender, EventArgs e) {
+            if (but_click == false) {
                 string com_name = comboPort.SelectedItem.ToString();
                 Box_log.AppendText("Выбран порт: " + com_name + "\r\n");
                 Connect(com_name);
-            }
-            else if (but_click == true)
-            {
+            } else if (but_click == true) {
                 Button_con.Text = "Соединение";
                 lab_statuscon.Text = "не подключено";
                 lab_statuscon.ForeColor = Color.Red;
@@ -56,9 +39,9 @@ namespace Program_for_diplom
             }
         }
 
-        private void Connect(string name)
-        {
-            comport.BaudRate = 38400;
+        private void Connect(string name) {
+            comport.PortName = name;
+            comport.BaudRate = 9600;
             comport.DataBits = 8;
             comport.Parity = Parity.None;
             comport.StopBits = StopBits.One;
@@ -69,59 +52,46 @@ namespace Program_for_diplom
             Box_log.AppendText("Длина данных:" + comport.DataBits.ToString() + "\r\n");
             Box_log.AppendText("Параметры порта: отсутсвует\r\nКоличество stop-битов: 1\r\n");
             Box_log.AppendText("Таймаут: 2с\r\nСоединение...\r\n");
-            try
-            {
+            try {
                 comport.Open();
-                if (comport.IsOpen == true)
-                {
+                if (comport.IsOpen == true) {
                     Box_log.AppendText("Порт открыт. Отправка запроса устройству:\r\n");
-                    for (int i = 1; i < 4; i++)
-                    {
+                    for (int i = 1; i < 4; i++) {
                         Box_log.AppendText("Попытка:" + i.ToString() + "\r\n");
                         Write_uart(Convert.ToByte('G'), Convert.ToByte('Y'), Convert.ToByte('B'));
-                        while (true)
-                        {
-                            if (Read == true)
-                            {
+                        while (true) {
+                            if (Read == true) {
                                 Read = false;
                                 break;
                             }
                         }
-                        if ((com_port_read[0] == 65) && (com_port_read[1] == 89) && (com_port_read[2] == 69))
-                        {
+                        Box_log.AppendText(_data);
+                        if ((com_port_read[0] == (byte)65) && (com_port_read[1] == (byte)86) && (com_port_read[2] == (byte)69)) {
                             Box_log.AppendText("Соединение установлено.\r\n");
                             Con = true;
                             Button_con.Text = "Разорвать";
                             lab_statuscon.Text = "соединено";
                             lab_statuscon.ForeColor = Color.Green;
                             break;
-                        }
-                        else
-                        {
+                        } else {
                             Box_log.AppendText("Отказ.\r\n");
                         }
                     }
-                    if(Con == false)
-                    {
+                    if (Con == false) {
                         Box_log.AppendText("Подключено неизвестное устройство. Переподключите еще раз или выберите другой порт.\r\n");
                         comport.Close();
                     }
-                }
-                else
-                {
+                } else {
                     Box_log.AppendText("Ошибка открытия порта. Попробуйте открыть порт еще раз.\r\n");
                     comport.Close();
                 }
-            }
-            catch(Exception ex)
-            {
+            } catch (Exception ex) {
                 Box_log.AppendText("Ошибка:" + ex.ToString() + "\r\n");
                 comport.Close();
             }
         }
 
-        private void Write_uart(byte comand, byte data1, byte data2)
-        {
+        private void Write_uart(byte comand, byte data1, byte data2) {
             com_port_write[0] = comand;
             com_port_write[1] = data1;
             com_port_write[2] = data2;
@@ -132,34 +102,28 @@ namespace Program_for_diplom
             comport.Write(com_port_write, 0, 3);
         }
 
-        private void Comport_DataReceived(object sender, SerialDataReceivedEventArgs e)
-        {
-            try
-            {
-                Box_log.AppendText("Чтение ответа:\r\n");
+        private void Comport_DataReceived(object sender, SerialDataReceivedEventArgs e) {
+            try {
+                _data = "Чтение ответа:\r\n";
                 int sizebuf = comport.BytesToRead;
-                if (sizebuf == 3) // проеверяем скок пришло, если больше или меньше, то тут что-то не так
+                if (sizebuf == 4) // тут 4 потому что у меня терминал в конце ставит /n, у тебя может быть по-другому
                 {
-                    for (int i = 0; i < sizebuf; i++)
-                    {
+                    _data = "";
+                    for (int i = 0; i < sizebuf - 1; i++) {
                         com_port_read[i] = (byte)comport.ReadByte();
-                        Box_log.AppendText(com_port_read.ToString() + " ");
+                        _data += $"{com_port_read[i]} ";
                     }
-                    Box_log.AppendText("\r\n");
+                    _data += "\r\n";
                     Read = true;
-                }
-                else
-                {
-                    Box_log.AppendText("Ошибка чтения буфера: количество байтов не совпадает с необходимым.\r\n");
+                } else {
+                    _data = "Ошибка чтения буфера: количество байтов не совпадает с необходимым.\r\n";
                     com_port_read[0] = Convert.ToByte('!');
                     com_port_read[1] = Convert.ToByte('!');
                     com_port_read[2] = Convert.ToByte('!');
                     Read = true;
                 }
-            }
-            catch(TimeoutException exc)
-            {
-                Box_log.AppendText("Ошибка чтения буфера:" + exc.ToString() + "\r\n");
+            } catch (TimeoutException exc) {
+                _data = $"Ошибка чтения буфера: {exc.ToString()}\r\n";
                 com_port_read[0] = Convert.ToByte('!');
                 com_port_read[1] = Convert.ToByte('!');
                 com_port_read[2] = Convert.ToByte('!');
